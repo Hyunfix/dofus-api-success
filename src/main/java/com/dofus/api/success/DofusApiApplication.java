bac608ae3c1e6438ea3f9b5486aa386e19c7cf26
Alexandre.B <alexandrebechet@hotmail.fr>
2022-04-13 20:48:31 +0200

    lancement application
    lancement application

---
+++ src/main/java/com/dofus/api/success/DofusApiApplication.java
@@ -0,0 +1,13 @@
+package com.dofus.api.success;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DofusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DofusApiApplication.class, args);
+	}
+
+}
