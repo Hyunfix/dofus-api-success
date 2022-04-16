f3a0a22f854bdeeb63811736ddd0a959c36d7815
Alexandre.B <alexandrebechet@hotmail.fr>
2022-04-16 22:14:10 +0200

    Log body
    mise en place log de la reponse body

--- src/main/java/com/dofus/api/config/CustomClientHttpRequestInterceptor.java
+++ src/main/java/com/dofus/api/config/CustomClientHttpRequestInterceptor.java
@@ -7,7 +7,11 @@ import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 public class CustomClientHttpRequestInterceptor implements ClientHttpRequestInterceptor {
 
@@ -17,15 +21,30 @@ public class CustomClientHttpRequestInterceptor implements ClientHttpRequestInte
 
     @Override
     public ClientHttpResponse intercept(
-            HttpRequest request, byte[] body,
+            HttpRequest request, byte[] reqBody,
             ClientHttpRequestExecution execution) throws IOException {
 
         logRequestDetails(request);
-        return execution.execute(request, body);
+
+        logResponseDetails(request, reqBody, execution);
+
+        return execution.execute(request, reqBody);
     }
+
+    private void logResponseDetails(HttpRequest request, byte[] reqBody, ClientHttpRequestExecution execution) throws IOException {
+        ClientHttpResponse response = execution.execute(request, reqBody);
+        InputStreamReader isr = new InputStreamReader(
+                response.getBody(), StandardCharsets.UTF_8);
+        String body = new BufferedReader(isr).lines()
+                .collect(Collectors.joining("\n"));
+        log.info("Response body: {}", body);
+    }
+
     private void logRequestDetails(HttpRequest request) {
         log.info("Headers: {}", request.getHeaders());
         log.info("Request Method: {}", request.getMethod());
         log.info("Request URI: {}", request.getURI());
     }
+
+
 }
